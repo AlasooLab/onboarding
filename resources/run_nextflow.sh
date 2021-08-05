f9b3172ec7d95c1df8d18c28a0d88968faaa9fa2
Kaur Alasoo <kaur.alasoo@gmail.com>
2021-08-05 09:25:43 +0300

    Create run_nextflow.sh

---
+++ resources/run_nextflow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=5G
+#SBATCH --job-name="genimpute"
+#SBATCH --partition=amd
+
+# Load needed system tools (Java 8 is required, one of singularity or anaconda - python 2.7 is needed,
+# depending on the method for dependancy management). The exact names of tool modules might depend on HPC.
+module load java-1.8.0_40
+module load python/2.7.15/native
+module load singularity/3.5.3
+module load squashfs/4.4
+
+nextflow run main.nf -profile tartu_hpc\
+   --studyFile testdata/multi_test.tsv\
+    --vcf_has_R2_field FALSE\
+    --run_nominal\
+    --run_permutation\
+    --run_susie\
+    --varid_rsid_map_file testdata/varid_rsid_map.tsv.gz\
+    --n_batches 25
+
