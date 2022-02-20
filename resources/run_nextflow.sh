6c0b4b84d50d245ae6c6ea03fdb0aab9968b8b03
Kaur Alasoo <kaur.alasoo@gmail.com>
2022-02-20 18:37:19 +0200

    Update run_nextflow.sh

--- resources/run_nextflow.sh
+++ resources/run_nextflow.sh
@@ -9,9 +9,9 @@
 
 # Load needed system tools (Java 8 is required, one of singularity or anaconda - python 2.7 is needed,
 # depending on the method for dependancy management). The exact names of tool modules might depend on HPC.
-module load java-1.8.0_40
-module load python/2.7.15/native
-module load singularity/3.5.3
+module load any/jdk/1.8.0_265
+module load nextflow
+module load any/singularity/3.5.3
 module load squashfs/4.4
 
 nextflow run main.nf -profile tartu_hpc\
